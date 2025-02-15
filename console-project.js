04c78e9a87a5fbcf5845583fa453371251dc76ac
Nuel King <166351729+engrigweemmanuel112@users.noreply.github.com>
2025-02-15 16:51:08 +0000

    initial commit

---
+++ console-project.js
@@ -0,0 +1,13 @@
+function productFunction (){
+  var product = "software developer"
+
+console.log("Software developer");
+};
+productFunction();
+
+// OR WE CAN YOU  FUNCTION
+
+var product = "cars";
+console.log(product);
+
+
